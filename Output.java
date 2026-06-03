ec6a96f730ffb41dd6ba3956e0763b4ce9c626da
gracemichelle5123-design <michelleadeagbo210@gmail.com>
2026-06-03 10:46:54 +0100

    Output

---
+++ Output.java
@@ -0,0 +1,17 @@
+public class Output{
+   public static void main(String[] args) {
+	   System.out.println("Hello everyone, this is my first java code ");
+	   System.out.println("Learning Java is fun");
+	   System.out.print("Here I am using the print method");
+	   System.out.print("This test will be appended");
+	   System.out.printf("%nMichelle is %d years ",26);
+	   System.out.printf("%nMy account balance is %f%n", 7657.999);
+	   System.out.printf("%nMy account balance is %,.2f%n", 7657.999);
+	   System.out.printf("%s is a boy%n", "Benjamin");
+	   System.out.printf("Do you love java? %b%n",true);
+	   System.out.printf("Fuel price in Nigeria has been increased by %%80%n");
+	   System.out.printf("My account balance is %c%,.2f%n",'$',7657.999965);
+	
+  }
+   
+} 
---
+++ Questions.java
@@ -0,0 +1,8 @@
+public class Questions{
+   public static void main(String[] args) {
+    System.out.println(" Good day ma , why not colon after the string in the bracket ? ");
+   }
+} 
+
+
+
