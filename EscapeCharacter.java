06d13494fdcd6df6424a1176556717f7c989ccb3
gracemichelle5123-design <michelleadeagbo210@gmail.com>
2026-06-09 10:27:18 +0100

    ...

---
+++ EscapeCharacter.java
@@ -0,0 +1,9 @@
+public class EscapeCharacter{
+	public static void main(String[] args) {
+		System.out.println("The father if Java is \"James Gosling\"");
+		System.out.println("To work with escape characters use slash \\ follow by the characters that you need");	
+			System.out.print("I am using an escape character here \r");
+	System.out.print("This is the carriage return text");
+	
+	}
+}
