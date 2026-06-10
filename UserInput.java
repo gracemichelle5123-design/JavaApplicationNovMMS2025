c077cc0b2a16f83ddf9c0bba40908b30bb8d08af
gracemichelle5123-design <michelleadeagbo210@gmail.com>
2026-06-10 10:08:57 +0100

    10th edited

--- UserInput.java
+++ UserInput.java
@@ -18,11 +18,24 @@ public class UserInput{
 		String address = input.nextLine();
 		
 		
+		System.out.print( "Enter number of student in you class ");
+		short numberOfStudent = input.nextShort();
+		
+		
+		System.out.print("Enter number of students in your school : ");
+		int studentInSchool = input.nextInt() ;
+		
+		System.out.print("Enter your gender (M/F): ");
+		char gender = input.next().charAt(0);
+		
 		
 		System.out.println("============================================\n\n");
 		System.out.printf("Hello %s, you are welcome to NIIT%n", fullName);
 		System.out.printf("%s you are %d years old%n", fullName,age);
+		System.out.printf("%s your gender is %c%n ",fullName,gender);
 		System.out.printf("You are living in %s%n", address);
+		System.out.printf("There are %d number of students in your class%n" , numberOfStudent);
+		System.out.printf("There are %,d student in my school%n", studentInSchool);
 		
 	}
 }
